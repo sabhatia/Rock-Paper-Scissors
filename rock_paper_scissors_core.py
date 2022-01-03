3006b9da43e158f6abe78a51fc056f8bb2ed6b74
Sandeep Bhatia <sabhatia@gmail.com>
2022-01-03 11:46:37 -0800

    Refactored core to remove extra functions

--- rock_paper_scissors_core.py
+++ rock_paper_scissors_core.py
@@ -19,23 +19,19 @@ def get_move_str(move_id):
     # Valid Id: Look into the list for the correct string
     return (moves[move_id - 1])
 
-def win_loss(drawA, drawB):
-    assert(drawA in moves)
-    assert(drawB in moves)
-
-    # If both are same, it's a draw
-    if drawB == drawA:
-        return "draw"
-
-    if drawB in rules[drawA]:
-        return "win"
-    else:
-        return "lose"
-
 def make_a_choice():
     # get a choice from the list of possible moves
     return(choice(moves))
    
-def evaluate_turn(playerA, playerB):
-    return win_loss(playerA, playerB)
+def evaluate_turn(choiceA, choiceB):
+    assert(choiceA in moves)
+    assert(choiceB in moves)
 
+    # If both are same, it's a draw
+    if choiceB == choiceA:
+        return "draw"
+
+    if choiceB in rules[choiceA]:
+        return "win"
+    else:
+        return "lose"
